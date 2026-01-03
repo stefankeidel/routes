352eae8e99ad62119add2eedbfb9b58dcf3012fb
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2026-01-03 23:47:08 +0100

    width

--- lib/routes_web/components/layouts.ex
+++ lib/routes_web/components/layouts.ex
@@ -63,7 +63,7 @@ defmodule RoutesWeb.Layouts do
     </header>
 
     <main class="px-4 py-20 sm:px-6 lg:px-8">
-      <div class="mx-auto max-w-2xl space-y-4">
+      <div class="mx-auto max-w-5xl space-y-4">
         {render_slot(@inner_block)}
       </div>
     </main>
